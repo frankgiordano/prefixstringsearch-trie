080a0493fa553d3cf4b640d64068e44d65a34b27
Frank Giordano <giofr01@yahoo.com>
2020-03-21 01:39:54 -0400

    reformat

--- src/TriePreFix.java
+++ src/TriePreFix.java
@@ -2,38 +2,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TriePreFix {
-	
-	private TrieNode root;
-	
-	public TriePreFix ()
-	{
-		root = new TrieNode();
-	}
-	
-	public TriePreFix(String[] words) {
-		root = new TrieNode();
-		root.addWords(words); 
-	}
 
-	public void addWord(String word) 
-	{
-		root.addWord(word);
-	}
-	
-	public List<String> getWords(String prefix) 
-	{
-		TrieNode lastNode = root;
-		int limit = prefix.length();
-		
-		// retrieve the last Node of the trie for 
-		// the last character of the prefix
-		for (int i = 0; i < limit; i++)
-		{
-			lastNode = lastNode.getNode(prefix.charAt(i) - 'a');
-			// if none, return empty array
-			if (lastNode == null) return new ArrayList<String>();
-		}
-		return lastNode.getWords();
-	}
+    private TrieNode root;
+
+    public TriePreFix() {
+        root = new TrieNode();
+    }
+
+    public TriePreFix(String[] words) {
+        root = new TrieNode();
+        root.addWords(words);
+    }
+
+    public void addWord(String word) {
+        root.addWord(word);
+    }
+
+    public List<String> getWords(String prefix) {
+        TrieNode lastNode = root;
+        int limit = prefix.length();
+
+        // retrieve the last Node of the trie for
+        // the last character of the prefix
+        for (int i = 0; i < limit; i++) {
+            lastNode = lastNode.getNode(prefix.charAt(i) - 'a');
+            // if none, return empty array
+            if (lastNode == null)
+                return new ArrayList<String>();
+        }
+        
+        return lastNode.getWords();
+    }
 
 }
--- src/TrieTest.java
+++ src/TrieTest.java
@@ -9,33 +9,30 @@ import java.io.IOException;
  * prefix. 
  * 
  */
-public class TrieTest 
-{
+public class TrieTest {
 
-	public static void main(String[] args) throws IOException 
-	{
-		String outString;
-		TriePreFix trie;
-		byte[] input;
-		
-		input = new byte[80];
-		System.out.print("Enter a set of words to define our word dictionary (separate by space only): ");
-		System.in.read(input);
-		outString = (new String(input, 0, input.length)).trim();
-		trie = new TriePreFix(outString.split(" "));
-		
-		do {
-		     input = new byte[80];
-		     System.out.print("Enter a Prefix string to search dictionary (press enter to quick): ");
-		     System.in.read(input);
-		     outString = (new String(input, 0, input.length)).trim();
-		     if (outString.length() > 0)
-		     {
-		    	 System.out.println(trie.getWords(outString).toString());
-		     }
-		 } while (outString.length() > 0);
-		
-		System.out.println("EXITED");
-	}
+    public static void main(String[] args) throws IOException {
+        String outString;
+        TriePreFix trie;
+        byte[] input;
+
+        input = new byte[80];
+        System.out.print("Enter a set of words to define our word dictionary (separate by space only): ");
+        System.in.read(input);
+        outString = (new String(input, 0, input.length)).trim();
+        trie = new TriePreFix(outString.split(" "));
+
+        do {
+            input = new byte[80];
+            System.out.print("Enter a Prefix string to search dictionary (press enter to quick): ");
+            System.in.read(input);
+            outString = (new String(input, 0, input.length)).trim();
+            if (outString.length() > 0) {
+                System.out.println(trie.getWords(outString).toString());
+            }
+        } while (outString.length() > 0);
+
+        System.out.println("EXITED");
+    }
 
 }
